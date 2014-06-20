151f8996642e21f6e29d566a59b6fb64babeefb8
steenzout <pedro.salgado@mediaforge.com>
2014-06-20 16:41:47 +0100

    implemented test cases for kerfi.commandline.print_properties().

--- tests/commandline_test.py
+++ tests/commandline_test.py
@@ -8,6 +8,51 @@ import tests
 from kerfi import commandline
 
 
+class PrintPropertiesTestCase(tests.BaseTestCase):
+    """
+    Test cases for kerfi.commandline.print_properties() function.
+    """
+
+    @mock.patch('sys.stdout.write', autospec=True)
+    @mock.patch('kerfi.load_raw')
+    def test_auto(self, mock_load_raw, mock_stdout):
+        """
+        Tests kerfi.commandline.print_properties() writes the expected message to stdout.
+        """
+        mock_load_raw.return_value = {
+            'other_prop': '0', 'prop1': '1', 'prop2': '2', 'single': 3}
+
+        with open('tests/input.txt', 'r') as f:
+            commandline.print_properties(f, ['single', 'prop.*', 'none'])
+
+            mock_load_raw.assert_called_once(f)
+
+        mock_stdout.assert_has_calls([
+            mock.call('prop1\t1\n'),
+            mock.call('prop2\t2\n'),
+            mock.call('single\t3\n')], any_order=True)
+
+
+    @mock.patch('sys.stdout.write', autospec=True)
+    @mock.patch('kerfi.load_xml')
+    def test_xml(self, mock_load_xml, mock_stdout):
+        """
+        Tests kerfi.commandline.print_properties() writes the expected message to stdout.
+        """
+        mock_load_raw.return_value = {
+            'other_prop': '0', 'prop1': '1', 'prop2': '2', 'single': 3}
+
+        with open('tests/input.txt', 'r') as f:
+            commandline.print_properties(f, ['single', 'prop.*', 'none'], format=kerfi.commandline.XML)
+
+            mock_load_xml.assert_called_once(f)
+
+        mock_stdout.assert_has_calls([
+            mock.call('prop1\t1\n'),
+            mock.call('prop2\t2\n'),
+            mock.call('single\t3\n')], any_order=True)
+
+
 class PrintVersionTestCase(tests.BaseTestCase):
     """
     Test cases for kerfi.commandline.print_version() function.
