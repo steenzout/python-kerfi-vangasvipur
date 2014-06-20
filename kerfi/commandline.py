d99443a016eb86e46470fe946d42cbdc3bd8dbde
steenzout <pedro.salgado@mediaforge.com>
2014-06-20 16:41:21 +0100

    implemented kerfi.commandline.print_properties().

--- kerfi/commandline.py
+++ kerfi/commandline.py
@@ -6,30 +6,67 @@
 .. moduleauthor:: Pedro Salgado <steenzout@ymail.com>
 """
 
+from __future__ import absolute_import
+
+
 import sys
 
 import kerfi
+
+import logging
+
+import re
 
 
 AUTO = 'auto'
 RAW = 'raw'
 XML = 'xml'
 
+LOGGER = logging.getLogger(__name__)
 
-def print_properties(stream, properties, format=AUTO):
-    if format != AUTO:
-        fmt = format
+
+def print_properties(stream, properties, input_format=AUTO):
+    """
+    Prints the given system_profiler properties to stdout.
+
+    :param stream: the input stream.
+    :param properties: the system properties to be printed.
+    :type properties: list
+    :param input_format: the input format (auto, raw or xml).
+    :type input_format: str
+    """
+
+    if input_format == AUTO:
+        header = stream.readline(5)
+        stream.seek(0)
+        if header.startswith('<?xml'):
+            fmt = XML
+        else:
+            fmt = RAW
     else:
-        fmt = None
+        fmt = input_format
 
-    for line in stream.readlines():
-        if fmt is None:
-            if line.startswith('<?xml'):
-                fmt = XML
-            else:
-                fmt = RAW
+    if fmt == RAW:
+        data = kerfi.load_raw(stream)
+    else:
+        data = kerfi.load_xml(stream)
 
-        print '%s : %s' % (fmt, line)
+    LOGGER.debug('input_format=%s fmt=%s', input_format, fmt)
+
+    for key, value in data.iteritems():
+        LOGGER.debug('key %s in properties (%s)? %s', key, properties, key in properties)
+
+        if key in properties:
+            LOGGER.debug('key:value = %s:%s', key, value)
+            sys.stdout.write('%s\t%s\n' % (key, value))
+        else:
+            for prop in properties:
+                LOGGER.debug('re.match %s matches %s? %s', prop, key, re.match(prop, key) is not None)
+
+                if re.match(prop, key):
+                    LOGGER.debug('sys.stdout.write(\'%s\t%s\n\')', key, value)
+                    LOGGER.debug('key:value = %s:%s', key, value)
+                    sys.stdout.write('%s\t%s\n' % (key, value))
 
 
 def print_version():
