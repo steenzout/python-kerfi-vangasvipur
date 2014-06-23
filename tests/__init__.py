eb7cad4dd228cb937bb0a040027b018c143b2fed
steenzout <pedro.salgado@mediaforge.com>
2014-06-23 18:42:24 +0100

    moved logging configuration to tests package.

--- tests/__init__.py
+++ tests/__init__.py
@@ -9,14 +9,39 @@
 from __future__ import absolute_import
 
 
-import logging
+import os
 
-import tests.logging
+import logging
+import logging.config
 
 import unittest
 
 
-tests.logging.load_configuration()
+DEFAULT_CONFIG_FILE = 'tests/logging.conf'
+
+
+def load_logging_configuration(config_file=DEFAULT_CONFIG_FILE):
+    """
+    Loads logging configuration from the given configuration file.
+
+    :param config_file: the configuration file (default=tests/logging.conf)
+    :type config_file: str
+    """
+    if not os.path.exists(config_file) or not os.path.isfile(config_file):
+        msg = '%s configuration file does not exist!', config_file
+        logging.getLogger(__name__).error(msg)
+        raise ValueError(msg)
+
+    try:
+        logging.config.fileConfig(config_file, disable_existing_loggers=False)
+        logging.getLogger(__name__).info('%s configuration file was loaded.', config_file)
+    except StandardError as error:
+        logging.getLogger(__name__).error('Failed to load configuration from %s!', config_file)
+        logging.getLogger(__name__).debug(str(error), exc_info=True)
+        raise error
+
+
+load_logging_configuration()
 
 
 class Basic(object):
